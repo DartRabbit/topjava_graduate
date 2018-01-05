5b20b6a23f11a7259a9b3d185e6d5558fa5fbd6c
DartRabbit <dartrabbit02@gmail.com>
2018-01-05 16:18:32 +0300

    authorized user, jackson object mapper, spring mvc config, jackson dependencies

---
+++ src/main/java/restaurant/rating/AuthorizedUser.java
@@ -0,0 +1,16 @@
+package restaurant.rating;
+
+import restaurant.rating.model.AbstractBaseEntity;
+
+public class AuthorizedUser {
+    private static int id = AbstractBaseEntity.START_SEQ;
+
+    public static int id() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        AuthorizedUser.id = id;
+    }
+
+}
