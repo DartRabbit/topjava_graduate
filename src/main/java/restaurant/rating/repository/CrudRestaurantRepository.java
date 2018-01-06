d8ea4ec87b2f1a1c91e3f75ddbcf420c41eb0f3a
DartRabbit <dartrabbit02@gmail.com>
2018-01-06 09:32:02 +0300

    restaurant repository

---
+++ src/main/java/restaurant/rating/repository/CrudRestaurantRepository.java
@@ -0,0 +1,38 @@
+package restaurant.rating.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import restaurant.rating.model.Restaurant;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional(readOnly = true)
+public interface CrudRestaurantRepository extends JpaRepository<Restaurant, Integer> {
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Restaurant r WHERE r.id=:id")
+    int delete(@Param("id") int id);
+
+    @Override
+    @Transactional
+    Restaurant save(Restaurant restaurant);
+
+    @Override
+    Optional<Restaurant> findById(Integer id);
+
+    @Override
+    List<Restaurant> findAll(Sort sort);
+
+    @Override
+    Page<Restaurant> findAll(Pageable pageable);
+
+    Restaurant findByName(String name);
+}
---
+++ src/main/java/restaurant/rating/repository/impl/DataJPARestaurantRepository.java
@@ -0,0 +1,47 @@
+package restaurant.rating.repository.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Repository;
+import restaurant.rating.model.Restaurant;
+import restaurant.rating.repository.CrudRestaurantRepository;
+
+import java.util.List;
+
+@Repository
+public class DataJPARestaurantRepository {
+    public static final Sort SORT_NAME = new Sort(Sort.Direction.ASC, "name");
+
+    @Autowired
+    CrudRestaurantRepository repository;
+
+
+    public Restaurant save(Restaurant restaurant) {
+        return repository.save(restaurant);
+    }
+
+
+    public boolean delete(int id) {
+        return repository.delete(id) != 0;
+    }
+
+
+    public Restaurant get(int id) {
+        return repository.findById(id).orElse(null);
+    }
+
+
+    public Restaurant getByName(String name) {
+        return repository.findByName(name);
+    }
+
+
+    public List<Restaurant> getAll() {
+        return repository.findAll(SORT_NAME);
+    }
+
+    public List<Restaurant> getPage(Pageable pageable) {
+        return repository.findAll(pageable).getContent();
+    }
+}
