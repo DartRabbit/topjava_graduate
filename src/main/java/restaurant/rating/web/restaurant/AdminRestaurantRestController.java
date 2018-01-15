14bddc0867fcbb72af8e464f1420289df0fd1c89
DartRabbit <dartrabbit02@gmail.com>
2018-01-15 11:18:56 +0300

    Codacy issues

--- src/main/java/restaurant/rating/web/restaurant/AdminRestaurantRestController.java
+++ src/main/java/restaurant/rating/web/restaurant/AdminRestaurantRestController.java
@@ -3,8 +3,6 @@ package restaurant.rating.web.restaurant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
