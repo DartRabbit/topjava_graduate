d66fd96fdd8a29e7c58094c491379d3080af10d3
DartRabbit <dartrabbit02@gmail.com>
2018-01-19 11:40:05 +0300

    Code review refactoring - delete users caching, fix README.md

--- src/main/java/restaurant/rating/repository/impl/DataJpaUserRepository.java
+++ src/main/java/restaurant/rating/repository/impl/DataJpaUserRepository.java
@@ -19,19 +19,16 @@ public class DataJpaUserRepository {
     private static final Sort SORT_NAME_EMAIL = new Sort(Sort.Direction.ASC, "name", "email");
     private final CrudUserRepository repository;
 
-    @CacheEvict(value = "users", allEntries = true)
     public User save(User user) {
         return repository.save(user);
     }
 
-    @CacheEvict(value = "users", allEntries = true)
     public User save(UserTo userTo) {
         User user = get(userTo.getId());
         return repository.save(UserUtil.updateFromTo(user, userTo));
     }
 
     // false if not found
-    @CacheEvict(value = "users", allEntries = true)
     public boolean delete(int id) {
         return repository.delete(id) != 0;
     }
@@ -47,7 +44,6 @@ public class DataJpaUserRepository {
     }
 
     // null if not found
-    @Cacheable("users")
     public List<User> getAll() {
         return repository.findAll(SORT_NAME_EMAIL);
     }
